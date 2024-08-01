9a1ad3ea915e576b33cf09072d6ea44784c0dbf7
CAPELLAX02 <ahmetatar002@gmail.com>
2024-08-01 04:01:55 +0300

    Dashboard Card added.

---
+++ components/dashboard/AnalyticsChart.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  ResponsiveContainer,
+} from 'recharts';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '../ui/card';
